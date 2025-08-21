91501fbf03a0407fa0d01b6504581a237dfdf32d
Lupo Oluwatobi Daniel <luponet07@gmail.com>
2025-08-21 11:43:59 +0100

    Update use-toast.ts

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -15,7 +15,7 @@ type ToasterToast = ToastProps & {
   id: string
   title?: React.ReactNode
   description?: React.ReactNode
-  action?: ToastActionElement
+  action?: ToastActionElementf
 }
 
 const actionTypes = {
